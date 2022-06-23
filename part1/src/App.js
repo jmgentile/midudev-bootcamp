3d1af04aefdf131a139eec533b227fff276d71d2
Juan <72452574+jmgentile@users.noreply.github.com>
2022-06-23 13:48:10 +0200

    created Message and Description components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import Message from './Message.js';
+import Description from './Description.js';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Message color="#212529" msg="Estamos trabajando..."/>
+      <Message color="#212529" msg="En un curso..."/>
+      <Message color="#212529" msg="De React."/>
+      <Description color="#4f5963" msg="Esta es la app del curso de Fullstack Bootcamp."/>
       </header>
     </div>
   );
---
+++ part1/src/Description.js
@@ -0,0 +1,5 @@
+const Description = (props) => {
+  return <p style={{color: props.color}}>{props.msg}</p>
+}
+
+export default Description
